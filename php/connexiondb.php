0695e621c62aefd8c8aa5536e3726f10bcb4f4e1
thomas serdjebi <thomas.serdjebi@laplateforme.io>
2021-11-25 14:40:33 +0100

    création fichier connexion BDD
    à tester

---
+++ php/connexiondb.php
@@ -0,0 +1,21 @@
+<?php
+
+//Déclaration des variables de connexion
+ 
+$BDD = array();
+$BDD ['host'] = "localhost" ;
+$BDD ['user'] = "root" ;
+$BDD ['pass'] = "" ;
+$BDD ['db'] = "thomas-serdjebi_livreor" ;
+$table = "utilisateurs";
+
+$mysqli = mysqli_connect ( $BDD ['host'], $BDD ['user'], $BDD ['pass'], $BDD ['db'] ) ;
+
+if (!$mysqli) {
+    echo "La connexion à la base de données a échouée";
+    $err_connexion = "La connexion à la base de données '".$BDD['db']."' a échoué ";
+}
+
+
+
+?>
