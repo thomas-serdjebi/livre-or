5f82bb9c67fc22df8629158c94315285451ccc99
thomas serdjebi <thomas.serdjebi@laplateforme.io>
2021-11-29 13:21:20 +0100

    COMMENTAIRE ==> CREATION STRUCTURE HTML FORM OK PHP A FAIRE

---
+++ php/commentaire.php
@@ -0,0 +1,79 @@
+<?php
+
+    session_start();
+
+    
+
+    require('connexiondb.php');
+
+   
+
+    if (!isset($_SESSION['login'])) {                                                       // SI PAS DUTILISATEUR CONNECTE
+        echo "Erreur, vous n'êtes pas connecté.";
+        $err_connexion = "Vous devez vous connecter pour accéder à cette page." ;
+
+    } 
+
+    // else { echo $_SESSION ['login']; 
+
+
+    
+
+
+?>
+
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Commentaire</title>
+        <!-- LINK LE CSS A FAIRE  -->
+
+    </head>
+
+    <body>
+
+        <!-- REQUIRE LE HEADER QUAND CREE -->
+
+        <main>
+            <section> 
+
+                <!-- TEXTE AVANT INFOS PROFIL -->
+
+            </section>
+
+            <section class="formsbox">
+
+                <!-- FORMULAIRE DE MODIFICATION LOGIN -->
+                
+                <div class="boxmodif">
+                    <div>
+                        <form action="commentaire.php" method="post" class="styleform">
+                            <div><textarea name='commentaire' rows='4' cols='55' placeholder='commentaire'></textarea></div>
+                            <div><input type='submit' name='commenter' value='Poster' ></div>
+                        </form>  
+                    </div>
+                </div>
+ 
+            </section>
+
+                
+
+        </main>
+
+        <!-- AJOUTER LE FOOTER REQUIRE -->
+        
+            
+    </body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
