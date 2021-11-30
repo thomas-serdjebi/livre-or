9ae500c18ef3100173a36721e98da64bf70b1488
thomas serdjebi <thomas.serdjebi@laplateforme.io>
2021-11-30 13:03:29 +0100

    INDEX => FONTS SELECTIONNES CSS EN COURS

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 
 
+
+
 ?>
 
 <!DOCTYPE html>
@@ -11,6 +13,9 @@
         <link rel="stylesheet" href="css/index.css">
         <link rel="stylesheet" href="css/header.css">
         <link rel="stylesheet" href="css/footer.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Lobster&family=Oleo+Script+Swash+Caps&display=swap" rel="stylesheet">
 
     </head>
 
